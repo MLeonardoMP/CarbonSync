c9de1e988133ad65a19c26c99a54bf7c72595c1b
Juan Mosquera <juanes.riveram@gmail.com>
2025-07-02 21:05:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/emissions-chart.tsx
+++ src/components/dashboard/emissions-chart.tsx
@@ -26,7 +26,7 @@ export function EmissionsChart<T extends object>({
       </CardHeader>
       <CardContent>
         <div className="h-[300px]">
-          <ResponsiveContainer width="100%" height="100%">
+          <ResponsiveContainer width="100%" height="100%" key={title}>
             <BarChart data={data}>
               <CartesianGrid strokeDasharray="3 3" vertical={false} />
               <XAxis
